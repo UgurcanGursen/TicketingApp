7d86258fc974e72dcf1705b1fa676c79a054d7d5
Ugurcan Gursen <ugurcangursen@gmail.com>
2019-12-27 14:43:10 +0300

    First Initialize

---
+++ src/test/java/com/ugurcangursen/ticketingapp/TicketingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugurcangursen.ticketingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
